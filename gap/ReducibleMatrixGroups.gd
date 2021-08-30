80488c62571f568f77eb0cfbceb4cf4a904bf567
Sergio Siccha <siccha@mathematik.uni-kl.de>
2021-08-30 15:09:16 +0200

    Docs Subspace stabilizer

--- gap/ReducibleMatrixGroups.gd
+++ gap/ReducibleMatrixGroups.gd
@@ -2,7 +2,7 @@
 #! @Section Maximal Subgroups
 #! @Description
 #! Return the subgroup of <M>SL(n, q)</M> stabilizing the
-#! <A>k</A>-dimensional subspace of <M>\mathbb{F}_q^n</M> consisting of
-#! all vectors whose first <C>n-k</C> entries are zero.
+#! <A>k</A>-dimensional subspace of <M>F^n</M>, where <C>F := GF(q)</C>,
+#! consisting of all vectors whose first <C>n-k</C> entries are zero.
 #! @Arguments n, q, k
 DeclareGlobalFunction("SLStabilizerOfSubspace");
