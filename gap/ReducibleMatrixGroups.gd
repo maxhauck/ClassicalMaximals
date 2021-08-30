27188de10cccfee96deba02a1fdbfc0e2bc9457d
mahauck <mahauck@rhrk.uni-kl.de>
2021-08-30 13:40:48 +0200

    change doc

--- gap/ReducibleMatrixGroups.gd
+++ gap/ReducibleMatrixGroups.gd
@@ -2,8 +2,8 @@
 #! @Chapter Functionality
 #! @Section Example Methods
 #! @Description
-#!   Construct the subgroup of $\mathrm{SL}(n, q)$ stabilising a
-#!   $k$-dimensional subspace of $\mathbb{F}_q^n$.
-#!   We take the $k$-space to be $(0,  \ldots,  0,  a_1,  \ldots,  a_k)$.
+#!   Return the subgroup of $\mathrm{SL}(n, q)$ stabilising the
+#!   $k$-dimensional subspace of $\mathbb{F}_q^n$ consisting of
+#!   all vectors whose first $n-k$ entries are zero.
 #! @Arguments n, q, k
 DeclareGlobalFunction("SLStabilizerOfSubspace");
