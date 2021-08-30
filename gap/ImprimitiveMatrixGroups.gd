c7d9905cd0071f979afd6ea1b8d80a368ae95fb4
mahauck <mahauck@rhrk.uni-kl.de>
2021-08-30 18:09:20 +0200

    docs

--- gap/ImprimitiveMatrixGroups.gd
+++ gap/ImprimitiveMatrixGroups.gd
@@ -2,8 +2,8 @@
 #! @Section Maximal Subgroups
 #! @Arguments n, q, t
 #! @Description
-#! Return the subgroup of $\mathrm{SL}(n, q)$ stabilising a
-#! decomposition $\mathbb{F}_q^n=V_1\oplus V_2\oplus\dots\oplus V_t$ 
-#! as a direct product where $\mathrm{dim}(V_1)=\mathrm{dim}(V_2)=\dots
-#! =\mathrm{dim}(V_t)$. Note that $t$ must be a divisor of $n$.
+#! Return the subgroup of <M>SL(n, q)</M> stabilising a
+#! decomposition <M>F^n=V_1\oplus V_2\oplus\dots\oplus V_t</M> of <M>F^n</M>,
+#! where <C>F := GF(q)</C>, as a direct sum of vector spaces of equal
+#! dimensions. Note that this means that <A>t</A> must be a divisor of <A>n</A>.
 DeclareGlobalFunction("ImprimitivesMeetSL");
