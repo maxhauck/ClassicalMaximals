94daa12011abf586dee4eba109f7411608afca41
mahauck <mahauck@rhrk.uni-kl.de>
2021-08-30 14:54:30 +0200

    update

--- gap/ReducibleMatrixGroups.gd
+++ gap/ReducibleMatrixGroups.gd
@@ -1,7 +1,7 @@
 #! @Chapter Utility Functions
 #! @Section Maximal Subgroups
 #! @Description
-#! Return the subgroup of <M>SL(n, q)</M> stabilising the
+#! Return the subgroup of <M>SL(n, q)</M> stabilizing the
 #! <A>k</A>-dimensional subspace of <M>\mathbb{F}_q^n</M> consisting of
 #! all vectors whose first <C>n-k</C> entries are zero.
 #! @Arguments n, q, k
